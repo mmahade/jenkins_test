1edc7b7f81ee4fa05b8e2552c9f9f60ea880589c
mahade <hasanmahadebd@gmail.com>
2025-04-10 16:39:23 +0600

    add hello test

--- src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
@@ -1,5 +1,6 @@
 package com.example.jenkins_test;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+	@PostConstruct
+	void helloFun() {
+		System.out.println("Hello World!");
 	}
 
 }
